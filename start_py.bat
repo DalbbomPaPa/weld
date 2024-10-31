9a7b051c70630e2cb93a5ef309a583fc768984a3
정철수 <charlie8966@gmail.com>
2024-10-31 21:58:44 +0800

    bat 추가

---
+++ start_py.bat
@@ -0,0 +1,5 @@
+:: start_script.bat
+@echo off
+cd C:\projects\welding_data\
+python polling_data.py
+pause
---
+++ start_server.bat
@@ -0,0 +1,4 @@
+:: start_django.bat
+@echo off
+cd C:\projects\welding_data\
+python manage.py runserver
