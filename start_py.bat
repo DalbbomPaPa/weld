2658642842a9add0ac36dfe108aae33ee0d359d0
정철수 <charlie8966@gmail.com>
2024-11-01 11:36:32 +0800

    db 포함

--- start_py.bat
+++
@@ -1,5 +0,0 @@
-:: start_script.bat
-@echo off
-cd C:\projects\welding_data\
-python polling_data.py
-pause
--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,14 @@
 :: start_django.bat
 @echo off
-cd C:\projects\welding_data\
-python manage.py runserver
+SET "VENV_PATH=C:\python\.venv"
+SET "DJANGO_PROJECT_PATH=C:\projects\welding_data\"
+
+
+REM 가상환경 활성화
+CALL %VENV_PATH%\Scripts\activate.bat
+
+REM Django 서버 실행
+start cmd /k "cd %DJANGO_PROJECT_PATH% && python manage.py runserver"
+
+REM 하위 스크립트 실행 (로그를 프롬프트에 표시)
+start cmd /k "cd %DJANGO_PROJECT_PATH% && python polling_data.py"
