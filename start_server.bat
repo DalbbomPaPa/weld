3410c74f3804cded690c5776119661ac2ba364b6
정철수 <charlie8966@gmail.com>
2024-11-01 15:35:06 +0800

    배치파일 변경

--- start_server.bat
+++ start_server.bat
@@ -12,3 +12,9 @@ start cmd /k "cd %DJANGO_PROJECT_PATH% && python manage.py runserver"
 
 REM 하위 스크립트 실행 (로그를 프롬프트에 표시)
 start cmd /k "cd %DJANGO_PROJECT_PATH% && python polling_data.py"
+
+REM 서버 시작 지연 (5초 대기)
+timeout /t 5 /nobreak >nul
+
+REM 브라우저에서 localhost:8000/weld 열기
+start edge.exe http://localhost:8000/weld
